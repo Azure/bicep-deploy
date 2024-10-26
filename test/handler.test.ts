3543245bd92a3491707c52c52279cbad0e3849da
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2024-10-26 08:31:13 -0400

    Fix lint issues

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -543,47 +543,71 @@ describe("stack execution", () => {
 });
 
 describe("validateFileScope", () => {
-  it('should ignore empty template', () => {
-    expect(() => validateFileScope({ scope: { type: "subscription" } } as any, {
-      templateContents: {}
-    })).not.toThrow();
+  it("should ignore empty template", () => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validateFileScope({ scope: { type: "subscription" } } as any, {
+        templateContents: {},
+      }),
+    ).not.toThrow();
   });
 
-  it('should ignore non-Bicep templates', () => {
-    expect(() => validateFileScope({ scope: { type: "subscription" } } as any, {
-      templateContents: {
-        '$schema': 'https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#',
-      }
-    })).not.toThrow();
+  it("should ignore non-Bicep templates", () => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validateFileScope({ scope: { type: "subscription" } } as any, {
+        templateContents: {
+          $schema:
+            "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+        },
+      }),
+    ).not.toThrow();
   });
 
-  it('should validate Bicep templates', () => {
-    expect(() => validateFileScope({ scope: { type: "subscription" } } as any, {
-      templateContents: {
-        '$schema': 'https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#',
-        metadata: { _generator: { name: 'bicep' } }
-      }
-    })).toThrow('The target scope resourceGroup does not match the deployment scope subscription.');
+  it("should validate Bicep templates", () => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validateFileScope({ scope: { type: "subscription" } } as any, {
+        templateContents: {
+          $schema:
+            "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+          metadata: { _generator: { name: "bicep" } },
+        },
+      }),
+    ).toThrow(
+      "The target scope resourceGroup does not match the deployment scope subscription.",
+    );
   });
 
   const schemaLookup: Record<ScopeType, string> = {
-    'tenant': 'https://schema.management.azure.com/schemas/2019-08-01/tenantDeploymentTemplate.json#',
-    'managementGroup': 'https://schema.management.azure.com/schemas/2019-08-01/managementGroupDeploymentTemplate.json#',
-    'subscription': 'https://schema.management.azure.com/schemas/2018-05-01/subscriptionDeploymentTemplate.json#',
-    'resourceGroup': 'https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#',
-  }
+    tenant:
+      "https://schema.management.azure.com/schemas/2019-08-01/tenantDeploymentTemplate.json#",
+    managementGroup:
+      "https://schema.management.azure.com/schemas/2019-08-01/managementGroupDeploymentTemplate.json#",
+    subscription:
+      "https://schema.management.azure.com/schemas/2018-05-01/subscriptionDeploymentTemplate.json#",
+    resourceGroup:
+      "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  };
 
-  const scopes: ScopeType[] = ['tenant', 'managementGroup', 'subscription', 'resourceGroup'];
-  for (const scope of scopes) {
-    it(`should validate ${scope} scope`, () => {
-      expect(() => validateFileScope({ scope: { type: scope } } as any, {
+  const scopes: ScopeType[] = [
+    "tenant",
+    "managementGroup",
+    "subscription",
+    "resourceGroup",
+  ];
+
+  it.each(scopes)("should validate %s scope", scope => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validateFileScope({ scope: { type: scope } } as any, {
         templateContents: {
-          '$schema': schemaLookup[scope as ScopeType],
-          metadata: { _generator: { name: 'bicep' } }
-        }
-      })).not.toThrow();
-    });
-  }
+          $schema: schemaLookup[scope as ScopeType],
+          metadata: { _generator: { name: "bicep" } },
+        },
+      }),
+    ).not.toThrow();
+  });
 });
 
 function getMockRestError(errorResponse: ErrorResponse) {
