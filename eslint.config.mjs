c04163b42bac3f0c4d299205b1c32df12d86e6e8
snehabandla <snehab@microsoft.com>
2025-11-20 11:59:04 -0800

    fixing linting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,32 +7,31 @@ import tseslint from "typescript-eslint";
 import vitest from '@vitest/eslint-plugin';
 import notice from "eslint-plugin-notice";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  files: ["src/**/*.ts", "test/**/*.ts", "test-live/**/*.ts", "packages/**/*.ts"],
-  extends: [
-    eslint.configs.recommended,
-    eslintPluginPrettierRecommended,
-    ...tseslint.configs.recommended,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: {
-      ...vitest.environments.env.globals,
-    }
-  },
-  plugins: {
-    notice,
-    vitest
-  },
-  rules: {
-    "notice/notice": [
-      "error",
-      {
-        template: "// Copyright (c) Microsoft Corporation.\n// Licensed under the MIT License.\n",
-      },
-    ],
-    ...vitest.configs.recommended.rules,
-  },
-});
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    files: ["src/**/*.ts", "test/**/*.ts", "test-live/**/*.ts", "packages/**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...vitest.environments.env.globals,
+      }
+    },
+    plugins: {
+      notice,
+      vitest
+    },
+    rules: {
+      "notice/notice": [
+        "error",
+        {
+          template: "// Copyright (c) Microsoft Corporation.\n// Licensed under the MIT License.\n",
+        },
+      ],
+      ...vitest.configs.recommended.rules,
+    },
+  }
+];
